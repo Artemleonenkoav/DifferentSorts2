cc584a45e1953a3cfd8e6a25445d4772901a064d
Artem <leonenkoavfin@gmail.com>
2022-05-17 13:07:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+// Чтобы перевернуть слово, поместите данное слово в стек (буква за буквой), а затем извлеките буквы из стека.
+
+// Code Example in Java Using a Recursion Function
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        System.out.print("Please enter the word or string: \n");
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+
+        System.out.print("Reversed string is: \n");
+        if(word.length() > 0) {
+            reverseString(word, word.length() - 1);
+        }
+    }
+
+    public static void reverseString(String s, int index) {
+        if(index == 0) {
+            System.out.println(s.charAt(index));
+            return;
+        }
+
+        System.out.print(s.charAt(index));
+        reverseString(s, index - 1);
+    }
+}
